fde9a138646c540552e622bb7040d990a8fdecc9
Nathaniel Kuga <natelkuga@protonmail.com>
2023-02-09 20:23:40 -0800

    Nathaniel: Added Project reference for the Test project, and also the NuGet package for reCAPTCHA.

---
+++ src/test/NUnitTests/Team121GBNUnitTests/ReCAPTCHATests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team121GBCapstoneProject;
+
+namespace Team121GBNUnitTests;
+
+public class AccountRegistrationCAPTCHATests
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void reCAPTCHAisPassed()
+    {
+        //setup
+        var reCAPTCHAvalue = false;
+
+        //arrange
+
+        //assert
+        Assert.That(reCAPTCHAvalue, Is.EqualTo(true));
+    }
+    [Test]
+    public void reCAPTCHAisNOTPassed()
+    {
+        //setup
+        var reCAPTCHAvalue = false;
+
+        //arrange
+        reCAPTCHAvalue = true; //temporary
+        //assert
+        Assert.That(reCAPTCHAvalue, Is.EqualTo(false));
+    }
+}
