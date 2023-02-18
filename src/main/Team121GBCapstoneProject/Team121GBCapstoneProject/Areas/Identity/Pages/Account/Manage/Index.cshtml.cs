f1b528986d42b5947fca05ffada13edb490e77d7
Cole Krieske <ckrieske@my.chemeketa.edu>
2023-02-17 20:32:38 -0800

    Added a place to put in a place to put a profile picture added it to the navbar

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -59,18 +59,22 @@ namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+            [Display(Name = "Profile Picture")]
+            public byte[] ProfilePicture { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var profilePicture = user.ProfilePicture;
 
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                ProfilePicture = profilePicture
             };
         }
 
@@ -109,6 +113,17 @@ namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account.Manage
                     StatusMessage = "Unexpected error when trying to set phone number.";
                     return RedirectToPage();
                 }
+            }
+
+            if (Request.Form.Files.Count > 0)
+            {
+                IFormFile file = Request.Form.Files.FirstOrDefault();
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    user.ProfilePicture = dataStream.ToArray();
+                }
+                await _userManager.UpdateAsync(user);
             }
 
             await _signInManager.RefreshSignInAsync(user);
