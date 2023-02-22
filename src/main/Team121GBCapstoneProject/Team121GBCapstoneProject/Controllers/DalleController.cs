33f77703b9e7d9e999bee70d180eeafe90904eb9
Cole Krieske <ckrieske@my.chemeketa.edu>
2023-02-22 13:15:04 -0800

    Added modal to Index page and linked to Dalle page

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/DalleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team121GBCapstoneProject.Controllers
+{
+    public class DalleController : Controller
+    {
+        public IActionResult GenerateImage()
+        {
+            return View();
+        }
+    }
+}
