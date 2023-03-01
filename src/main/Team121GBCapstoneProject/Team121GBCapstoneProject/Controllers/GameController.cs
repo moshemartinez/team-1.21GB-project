bfafbbf2043f3e649b36569b62606a74f4b25f73
Moshe Martinez <emartinez17@wou.edu>
2023-03-01 02:02:32 -0800

    Service class for searching implemented, will begin testing

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Team121GBCapstoneProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+    }
+}
