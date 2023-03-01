ffb69b550f5ad13ef9c7042e5c0454f625823605
Nathaniel Kuga <natelkuga@protonmail.com>
2023-02-28 19:32:35 -0800

    Nathaniel: Added the recaptcha method call back into Register.cshtml.cs

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Team121GBCapstoneProject.Areas.Identity.Data;
+using Team121GBCapstoneProject.Services;
 
 namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account
 {
@@ -30,13 +31,14 @@ namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-
+        private readonly IReCaptchaService _reCaptchaService;
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             IUserStore<ApplicationUser> userStore,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IReCaptchaService captchaService)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -44,6 +46,7 @@ namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _reCaptchaService = captchaService;
         }
 
         /// <summary>
@@ -110,8 +113,8 @@ namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
         }
 
-            //var user = CreateUser();
- 
+        //var user = CreateUser();
+
 
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -125,6 +128,11 @@ namespace Team121GBCapstoneProject.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
+                //
+                //check the recaptcha
+                if (!Request.Form.ContainsKey("g-recaptcha-response")) return Page();
+                var captcha = Request.Form["g-recaptcha-response"].ToString();
+                if (!await _reCaptchaService.IsValid(captcha)) return Page();
                 // Added code to add First Name and Last Name
                 var user = CreateUser();
                 user.FirstName = Input.FirstName;
