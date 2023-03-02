ece59dfd09beb5b12e0e905de353dea8902d42c4
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-01 18:34:47 -0800

    Nathaniel: Added a view model to hold the person object and their associated game lists.

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/ViewModels/GamesListsViewModel.cs
@@ -0,0 +1,9 @@
+using Team121GBCapstoneProject.Models;
+
+namespace Team121GBCapstoneProject.ViewModels;
+
+public class UserListsViewModel 
+{
+    public Person LoggedInUser { get; set; }
+    
+}
