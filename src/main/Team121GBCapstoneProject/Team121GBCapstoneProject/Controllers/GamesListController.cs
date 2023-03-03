769ac42a286d3b0d017b6e10c7835ee4354b48e4
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-03 10:59:22 -0800

    Nathaniel: currently working on building the form to add a list.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GamesListController.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GamesListController.cs
@@ -5,6 +5,7 @@ using Team121GBCapstoneProject.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Team121GBCapstoneProject.Areas.Identity.Data;
+using System.Diagnostics;
 
 namespace Team121GBCapstoneProjects.Controllers;
 
@@ -33,7 +34,7 @@ public class GamesListsController : Controller
         return View("Index", uservm);
     }
 
-    public IActionResult AddList(int userId, int listType)
+    public IActionResult AddList(int userId, int listType, string customListName)
     {
         // call the method in the personrepository to add the list
         var person = _personRepository.FindById(userId);
@@ -41,8 +42,28 @@ public class GamesListsController : Controller
         {
             return View("Error");
         }
+        var userLists = person.PersonGameLists.ToList();//.Contains(listType);
         
-        throw new NotImplementedException();
 
+        foreach(var checklist in userLists)
+        {
+            //If it is a custom list let them create it.
+            if(checklist.ListKindId == 4)
+            {
+                break;
+            }
+            if(checklist.ListKindId == listType)
+            {
+                Debug.WriteLine("User already has a list of this type");
+                return View("Index");
+            }
+        }
+
+        _personRepository.AddList(person, listType);
+        
+        var userVM = new UserListsViewModel();
+        userVM.LoggedInUser = person;
+
+        return View("Index", userVM);
     }
 }
