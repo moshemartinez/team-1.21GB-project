8d81cb20e4e8fb5e69ae19aa134fea6b52dbd8dc
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-05 19:01:27 -0800

    Nathaniel: Working on the logic for adding a game to a list where the game already exists in the db.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
@@ -8,6 +8,7 @@ using Team121GBCapstoneProject.Services.Abstract;
 using Team121GBCapstoneProject.DAL.Abstract;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
+using Team121GBCapstoneProject.DAL.Concrete;
 
 namespace Team121GBCapstoneProject.Controllers
 {
@@ -17,13 +18,19 @@ namespace Team121GBCapstoneProject.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IIgdbService _igdbService;
-        private IRepository<Game> _gameRepository;
+        private readonly IRepository<Person> _personRepository;
+        private readonly IRepository<Game> _gameRepository;
         private string _clientId;
         private string _bearerToken;
 
-        public GameController(IIgdbService igdbService, IRepository<Game> gameRepository)
+        public GameController(UserManager<ApplicationUser> userManager,
+                                IIgdbService igdbService, 
+                                IRepository<Person> personRepository, 
+                                IRepository<Game> gameRepository)
         {
+            _userManager = userManager;
             _igdbService = igdbService;
+            _personRepository = personRepository;
             _gameRepository = gameRepository;
         }
 
@@ -47,9 +54,12 @@ namespace Team121GBCapstoneProject.Controllers
             return Ok(searchResult);
         }
         [HttpPost("addGame")]
-        //public async Task<ActionResult<IgdbGame>> AddGameToList(Game game)
+        //public async Task<ActionResult<IgdbGame>> AddGameToList(Game game, string listName)
         public async Task<ActionResult<IgdbGame>> AddGameToList()
         {   //need to set it up so we have a user a that is logged in.
+            var loggedInUser = _personRepository.GetAll()
+                                               .Where(user => user.AuthorizationId == _userManager.GetUserId(User))
+                                               .First();
             var game = new Game();
             game.Title = "aaaaaaaaa";
             try
@@ -59,7 +69,22 @@ namespace Team121GBCapstoneProject.Controllers
                                             .Any(g => g.Title == game.Title);
                 if(check) //add game to a users list 
                 {
-                    
+                    check = loggedInUser.PersonGameLists
+                                        .Where(g => g.Game != null)
+                                        .Any(g => g.Game.Title == game.Title);
+                    //check = loggedInUser.PersonGameLists
+                    //                    .Where(g => g != null)
+                    //                    .Any(g => g.Game.Title == game.Title);
+                    //check if user's list already contains this game.
+                    if (check)
+                    {
+                        //return what?
+                        return Content("This game is already in your list.");
+                    }
+                    //grab game from db and add it your specified list
+                    string gameTitle = game.Title;
+                    game = _gameRepository.GetAll()
+                                          .Where(g => g.Title == gameTitle);
                 }
                 else // add game to db first.
                 {
