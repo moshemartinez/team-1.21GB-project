6c79279dfbbd4c9a36d0c8fb4f5455649dc549b1
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-05 18:11:43 -0800

    Nathaniel: Added logic to the controller to check if a game already exists in the db before adding it to a list. Also added the user mangager and genere Person repository to check if the user is logged in.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
@@ -2,12 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Team121GBCapstoneProject.Services;
+using Team121GBCapstoneProject.Areas.Identity.Data;
 using Team121GBCapstoneProject.Models;
 using Team121GBCapstoneProject.Services.Abstract;
 using Team121GBCapstoneProject.DAL.Abstract;
 using System.Diagnostics;
-
+using Microsoft.AspNetCore.Identity;
 
 namespace Team121GBCapstoneProject.Controllers
 {
@@ -15,6 +15,7 @@ namespace Team121GBCapstoneProject.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly IIgdbService _igdbService;
         private IRepository<Game> _gameRepository;
         private string _clientId;
@@ -45,15 +46,38 @@ namespace Team121GBCapstoneProject.Controllers
 
             return Ok(searchResult);
         }
+        [HttpPost("addGame")]
+        //public async Task<ActionResult<IgdbGame>> AddGameToList(Game game)
+        public async Task<ActionResult<IgdbGame>> AddGameToList()
+        {   //need to set it up so we have a user a that is logged in.
+            var game = new Game();
+            game.Title = "aaaaaaaaa";
+            try
+            {
+                // * check if this game already exists in the database
+                bool check = _gameRepository.GetAll()
+                                            .Any(g => g.Title == game.Title);
+                if(check) //add game to a users list 
+                {
+                    
+                }
+                else // add game to db first.
+                {
+                    await AddGameToDb(game);
+                    //now add to users list
+                }
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return BadRequest(e.Message);
+            }
+        }
 
         [HttpPost]
-        public async Task<ActionResult<IgdbGame>>  AddGame(Game game)
-        //public async Task<ActionResult<IgdbGame>>  AddGame()
+        public async Task<ActionResult<IgdbGame>>  AddGameToDb(Game game)
         {
-            game.Title = "Thing";
-            game.Description = "Thing Thing";
-            game.EsrbratingId = 9;
-            game.AverageRating =  9.0;
             try
             {
                 _gameRepository.AddOrUpdate(game);
@@ -65,6 +89,5 @@ namespace Team121GBCapstoneProject.Controllers
                 return BadRequest(e);
             }
         }
-
     }
 }
