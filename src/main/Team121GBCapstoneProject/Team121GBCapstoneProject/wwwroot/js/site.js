31786b3e197c9b029b7a7cb558af23573c982f32
Moshe Martinez <emartinez17@wou.edu>
2023-03-05 16:07:33 -0800

    Can navigate manually to the search feature and search bar works when on the search results page

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/site.js
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#searchButton").click(function (event) {
+        event.preventDefault(); // prevent the default form submission behavior
+        var query = $("#searchInput").val(); // get the value from the search input field
+        $.ajax({
+            type: "GET",
+            url: "/api/Game",
+            data: { query: query }, // pass the search query as a parameter to the API Controller
+            dataType: "json",
+            success: function (data) {
+                $("#gameTableBody").empty(); // clear the table body before populating with new data
+                $.each(data, function (i, game) {
+                    var row = "<tr><td>" + game.gameCoverArt + "</td><td>" + game.gameTitle + "</td><td>" + game.gameWebsite + "</td></tr>";
+                    $("#gameTableBody").append(row);
+                });
+            }
+        });
+    });
+});
