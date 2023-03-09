b1add013a629b02a53bf2756f213e89c775439f3
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-08 19:49:50 -0800

    Nathaniel: Setting up unit tests for ReCaptchaV3 service class.

---
+++ src/main/Team121GBCapstoneProject/Team121GBNUinitTest/ReCAPTCHAV3Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team121GBNUnitTest;
+
+public class ReCAPTCHAV3Tests
+{
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [Test]
+    public void ReCaptchaV3_Success_ShouldReturnTrue()
+    {
+        // * Arrange
+
+        // ! Act
+
+        // ? Assert
+    }
+    [Test]
+    public void ReCaptchaV3_Failure_ShouldReturnFalse()
+    {
+        // * Arrange
+
+        // ! Act
+
+        // ? Assert
+    }
+    [Test]
+    public void ReCaptchaV3_Invalid_ShouldReturnFalse()
+    {
+        // * Arrange
+
+        // ! Act
+
+        // ? Assert
+    }
+}
