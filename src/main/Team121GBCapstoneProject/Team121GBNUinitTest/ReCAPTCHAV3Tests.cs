edaeebe66334c80f7759e98680227bc460070837
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-09 10:53:40 -0800

    Nathaniel: set up the unit tests for the new ReCaptchV3Service class.

--- src/main/Team121GBCapstoneProject/Team121GBNUinitTest/ReCAPTCHAV3Tests.cs
+++ src/main/Team121GBCapstoneProject/Team121GBNUinitTest/ReCAPTCHAV3Tests.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Moq;
+using Moq.Contrib.HttpClient;
+using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team121GBCapstoneProject.Services.Concrete;
 
 namespace Team121GBNUnitTest;
 
@@ -18,27 +22,48 @@ public class ReCAPTCHAV3Tests
     public void ReCaptchaV3_Success_ShouldReturnTrue()
     {
         // * Arrange
-
+        var handler = new Mock<HttpMessageHandler>();
+        handler.SetupAnyRequest()
+               .ReturnsResponse(System.Net.HttpStatusCode.OK);
+        var reCaptchaV3 = new ReCaptchaV3Service(handler.CreateClientFactory());
+        var reCaptchaResponse = "{\"success\": true}";
+        var secretKey = "SecretKey";
         // ! Act
+        bool result = reCaptchaV3.VerifyAsync(reCaptchaResponse).Result;
 
         // ? Assert
+        Assert.That(result, Is.EqualTo(true));
     }
     [Test]
     public void ReCaptchaV3_Failure_ShouldReturnFalse()
     {
         // * Arrange
-
+        var handler = new Mock<HttpMessageHandler>();
+        handler.SetupAnyRequest()
+               .ReturnsResponse(System.Net.HttpStatusCode.OK);
+        var reCaptchaV3 = new ReCaptchaV3Service(handler.CreateClientFactory());
+        var reCaptchaResponse = "{\"success\": false}";
+        var secretKey = "SecretKey";
         // ! Act
+        bool result = reCaptchaV3.VerifyAsync(reCaptchaResponse).Result;
 
         // ? Assert
+        Assert.That(result, Is.EqualTo(false));
     }
     [Test]
     public void ReCaptchaV3_Invalid_ShouldReturnFalse()
     {
         // * Arrange
-
+        var handler = new Mock<HttpMessageHandler>();
+        handler.SetupAnyRequest()
+               .ReturnsResponse(System.Net.HttpStatusCode.OK);
+        var reCaptchaV3 = new ReCaptchaV3Service(handler.CreateClientFactory());
+        var reCaptchaResponse = "{}";
+        var secretKey = "SecretKey";
         // ! Act
+        bool result = reCaptchaV3.VerifyAsync(reCaptchaResponse).Result;
 
         // ? Assert
+        Assert.That(result, Is.EqualTo(false));
     }
 }
