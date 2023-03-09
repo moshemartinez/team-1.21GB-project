b3ef9d39d3c0242751ebc8b80d07c5f168e506ee
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-09 10:54:03 -0800

    Nathaniel: Setting up the new service classes.

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Services/Concrete/ReCaptchaV3Service.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCore.ReCaptcha;
+
+namespace Team121GBCapstoneProject.Services.Concrete;
+
+public class ReCaptchaV3Service : IReCaptchaService
+{
+    private readonly string _secretKey;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public Task<ReCaptchaResponse> GetVerifyResponseAsync(string reCaptchaResponse)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> VerifyAsync(string reCaptchaResponse, string action = null)
+    {
+        throw new NotImplementedException();
+    }
+}
