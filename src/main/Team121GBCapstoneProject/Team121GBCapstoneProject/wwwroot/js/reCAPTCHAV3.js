ad2f75ae37252f4120c71f3a93576afa5e5b8fdd
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-09 16:45:00 -0800

    Nathaniel: Added recaptcha v3 to the login page.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/reCAPTCHAV3.js
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/reCAPTCHAV3.js
@@ -9,3 +9,23 @@ $(function () {
         });
     });
 });
+
+$('form').on('submit', function (e) {
+    if (grecaptcha.getResponse() == "") {
+        e.preventDefault();
+        alert("Please complete the CAPTCHA before submitting the form.");
+    }
+    else {
+        console.log("reCAPTCHA passed on client side.");
+    }
+});
+
+$(function () {
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6LcR_-YkAAAAAIR4vU4A-D3bq9NjQvyv129J7Azj', { action: 'submit' }).then(function (token) {
+            // Add your logic to submit to your backend server here.
+            console.log(token);
+            $("#recaptcha").val(token);
+        });
+    });
+});
