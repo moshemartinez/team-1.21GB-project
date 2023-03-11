2ee557cd6fe60a0eedd60ba89e32365b67cc5711
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-11 13:46:16 -0800

    Nathaniel: Building the VM constructor.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Models/PersonListVM.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Models/PersonListVM.cs
@@ -4,9 +4,14 @@ public class PersonListVM
 {
     public List<List<PersonList>> PersonLists { get; set; }
 
-    public PersonListVM()
+    public PersonListVM(List<PersonList> personLists)
     {
+        PersonLists = new List<List<PersonList>>();
+        var t = personLists.Distinct().ToList();
+        // todo: construct 2d list
+            // List<PersonList> temp = personLists.Where(l => l.ListKind == i)
+            //                                    .ToList();
+            // PersonLists.Add(temp);
         
     }
-    
 }
