83f1456be4f591114b170071f04b819d04b37a12
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-11 14:26:40 -0800

    Nathaniel: Got the passing of a list of PersonListVM's to the view working properly.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Models/PersonListVM.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Models/PersonListVM.cs
@@ -2,13 +2,16 @@ namespace Team121GBCapstoneProject.Models;
 
 public class PersonListVM
 {
-    //public List PersonLists { get; set; }
     public string ListKind { get; set; }
-    public PersonListVM(string listKind, List<Game> games)
+    public PersonListVM(string listKind, List<PersonGame> games)
     {
-        
+        ListKind = listKind;
     }
 
+}
+// var distinctPeople = personLists.Distinct(p => new { p })
+//                                         .ToList();
+    //public List PersonLists { get; set; }
     // public PersonListVM(List<PersonList> personLists)
     // {
     //     PersonLists = personLists.OrderBy(l => l.ListKindId)
@@ -19,6 +22,3 @@ public class PersonListVM
     //                           .Distinct()
     //                           .ToList();
     // }
-}
-// var distinctPeople = personLists.Distinct(p => new { p })
-//                                         .ToList();
