3dea5503016088bd8a836a35b9ea0176544f57df
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-11 16:45:33 -0800

    Nathaniel: Got the javascript working for presenting the users lists.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/GameController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Team121GBCapstoneProject.Areas.Identity.Data;
 using Team121GBCapstoneProject.Models;
+using Team121GBCapstoneProject.Models.DTO;
 using Team121GBCapstoneProject.Services.Abstract;
 using Team121GBCapstoneProject.DAL.Abstract;
 using System.Diagnostics;
@@ -20,20 +21,24 @@ namespace Team121GBCapstoneProject.Controllers
         private readonly IIgdbService _igdbService;
         private readonly IRepository<Person> _personRepository;
         private readonly IRepository<Game> _gameRepository;
+        private readonly IPersonListRepository _personListRepository;
+
         private string _clientId;
         private string _bearerToken;
         private readonly IConfiguration _config;
 
         public GameController(UserManager<ApplicationUser> userManager,
                                 IIgdbService igdbService, 
-                                IRepository<Person> personRepository, 
+                                IRepository<Person> personRepository,                                 
                                 IRepository<Game> gameRepository, 
+                                IPersonListRepository personListRepository,
                                 IConfiguration configuration)
         {
             _userManager = userManager;
             _igdbService = igdbService;
             _personRepository = personRepository;
             _gameRepository = gameRepository;
+            _personListRepository = personListRepository;
             _config = configuration;
         }
 
@@ -56,102 +61,149 @@ namespace Team121GBCapstoneProject.Controllers
             return Ok(searchResult);
         }
 
-        [HttpPost("addGame")]
-        public async Task<ActionResult<IgdbGame>> AddGameToList(Game game, string listName)
-        //public async Task<ActionResult<IgdbGame>> AddGameToList(string listName)
-        {   //need to set it up so we have a user a that is logged in.
-            var loggedInUser = _personRepository.GetAll()
-                                               .Where(user => user.AuthorizationId == _userManager.GetUserId(User))
-                                               .First();
-            try
-            {
-                // * check if this game already exists in the database
-                bool check = _gameRepository.GetAll()
-                                            .Any(g => g.Title == game.Title);
-                if(check) //add game to a users list 
-                {
-                    check = loggedInUser.PersonGameLists
-                                        .Where(g => g.Game != null)
-                                        .Any(g => g.Game.Title == game.Title);
-                    //check if user's list already contains this game.
-                    if (check)
-                    {
-                        //return what?
-                        return Content("This game is already in your list.");
-                    }
-                    //grab game from db and add it your specified list
-                    string gameTitle = game.Title;
-                    game = _gameRepository.GetAll()
-                                          .Where(g => g.Title == gameTitle)
-                                          .First();
-                    //string listName = "Currently Playing";
-                    PersonGameList list = loggedInUser.PersonGameLists
-                                                      .Where(l => l.ListName.NameOfList == listName)
-                                                      .First();
+        [HttpGet("getUserLists")]
+        public async Task<ActionResult<List<PersonList>>> GetUserLists()
+        {
+            ApplicationUser currentUser = await _userManager.GetUserAsync(User);
+            List<PersonList> personLists = _personListRepository.GetAll()
+                                                                .Where(l => l.Person.AuthorizationId == currentUser.Id)
+                                                                .ToList();
+            List<PersonListDTO> personListDTOs = new List<PersonListDTO>();
+            personLists.ForEach(list => personListDTOs.Add(new PersonListDTO(list.ListKind)));
+            // foreach(PersonList list in personLists)
+            // {
+            //     personListDTOs.Add(new PersonListDTO(list.ListKind));
+            // }
+            return Ok(personListDTOs);
 
-                    PersonGameList gameList = new PersonGameList();
-                    gameList.ListName = list.ListName;
-                    gameList.ListNameId = list.ListNameId;
-                    gameList.Game = game;
-                    gameList.GameId = game.Id;
-                    gameList.Person = loggedInUser;
-                    gameList.PersonId = loggedInUser.Id;
-                    gameList.ListKind = list.ListKind;
-                    gameList.ListKindId = list.ListKindId;
-
-                    loggedInUser.PersonGameLists.Add(gameList);
-                    _personRepository.AddOrUpdate(loggedInUser);
-
-                }
-                else // add game to db first.
-                {
-                    await AddGameToDb(game);
-                    //now add to users list
-                    //grab game from db and add it your specified list
-                    string gameTitle = game.Title;
-                    game = _gameRepository.GetAll()
-                                          .Where(g => g.Title == gameTitle)
-                                          .First();
-                    //string listName = "Currently Playing";
-                    PersonGameList list = loggedInUser.PersonGameLists
-                                                      .Where(l => l.ListName.NameOfList == listName)
-                                                      .First();
-
-                    PersonGameList gameList = new PersonGameList();
-                    gameList.ListName = list.ListName;
-                    gameList.ListNameId = list.ListNameId;
-                    gameList.Game = game;
-                    gameList.GameId = game.Id;
-                    gameList.Person = loggedInUser;
-                    gameList.PersonId = loggedInUser.Id;
-                    gameList.ListKind = list.ListKind;
-                    gameList.ListKindId = list.ListKindId;
-
-                    loggedInUser.PersonGameLists.Add(gameList);
-                    _personRepository.AddOrUpdate(loggedInUser);
-                }
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e);
-                return BadRequest(e.Message);
-            }
+            // List<PersonGameList> userLists = _personRepository.GetAll()
+            //                                              .Where(user => user.AuthorizationId == currentUser.Id)
+            //                                              .First()
+            //                                              .PersonGameLists
+            //                                              .ToList();
+            // List<PersonGameListDTO> userListDTO = new List<PersonGameListDTO>();
+            // foreach (var list in userLists)
+            // {
+            //     PersonGameListDTO listDTO = new PersonGameListDTO(list.ListName.NameOfList);
+            //     userListDTO.Add(listDTO);  
+            // }
+            // if(userLists.Count == 0)
+            // {
+            //     return BadRequest("You don't have any lists!");
+            // }
+            // return Ok(userListDTO);
         }
-
-        [HttpPost]
-        public async Task<ActionResult<IgdbGame>>  AddGameToDb(Game game)
+        
+        [HttpPost("addGame")]
+        public async Task<ActionResult<IgdbGame>> addGameToList(Game game, string listKind)
         {
             try
             {
-                _gameRepository.AddOrUpdate(game);
+                // * check if game exists in db.
                 return Ok();
             }
             catch (Exception e)
             {
                 Debug.WriteLine(e);
-                return BadRequest(e);
+                return BadRequest();
             }
         }
+        // [HttpPost("addGame")]
+        // public async Task<ActionResult<IgdbGame>> AddGameToList(Game game, string listName)
+        // //public async Task<ActionResult<IgdbGame>> AddGameToList(string listName)
+        // {   //need to set it up so we have a user a that is logged in.
+        //     var loggedInUser = _personRepository.GetAll()
+        //                                        .Where(user => user.AuthorizationId == _userManager.GetUserId(User))
+        //                                        .First();
+        //     try
+        //     {
+        //         // * check if this game already exists in the database
+        //         bool check = _gameRepository.GetAll()
+        //                                     .Any(g => g.Title == game.Title);
+        //         if(check) //add game to a users list 
+        //         {
+        //             check = loggedInUser.PersonGameLists
+        //                                 .Where(g => g.Game != null)
+        //                                 .Any(g => g.Game.Title == game.Title);
+        //             //check if user's list already contains this game.
+        //             if (check)
+        //             {
+        //                 //return what?
+        //                 return Content("This game is already in your list.");
+        //             }
+        //             //grab game from db and add it your specified list
+        //             string gameTitle = game.Title;
+        //             game = _gameRepository.GetAll()
+        //                                   .Where(g => g.Title == gameTitle)
+        //                                   .First();
+        //             //string listName = "Currently Playing";
+        //             PersonGameList list = loggedInUser.PersonGameLists
+        //                                               .Where(l => l.ListName.NameOfList == listName)
+        //                                               .First();
+
+        //             PersonGameList gameList = new PersonGameList();
+        //             gameList.ListName = list.ListName;
+        //             gameList.ListNameId = list.ListNameId;
+        //             gameList.Game = game;
+        //             gameList.GameId = game.Id;
+        //             gameList.Person = loggedInUser;
+        //             gameList.PersonId = loggedInUser.Id;
+        //             gameList.ListKind = list.ListKind;
+        //             gameList.ListKindId = list.ListKindId;
+
+        //             loggedInUser.PersonGameLists.Add(gameList);
+        //             _personRepository.AddOrUpdate(loggedInUser);
+
+        //         }
+        //         else // add game to db first.
+        //         {
+        //             await AddGameToDb(game);
+        //             //now add to users list
+        //             //grab game from db and add it your specified list
+        //             string gameTitle = game.Title;
+        //             game = _gameRepository.GetAll()
+        //                                   .Where(g => g.Title == gameTitle)
+        //                                   .First();
+        //             //string listName = "Currently Playing";
+        //             PersonGameList list = loggedInUser.PersonGameLists
+        //                                               .Where(l => l.ListName.NameOfList == listName)
+        //                                               .First();
+
+        //             PersonGameList gameList = new PersonGameList();
+        //             gameList.ListName = list.ListName;
+        //             gameList.ListNameId = list.ListNameId;
+        //             gameList.Game = game;
+        //             gameList.GameId = game.Id;
+        //             gameList.Person = loggedInUser;
+        //             gameList.PersonId = loggedInUser.Id;
+        //             gameList.ListKind = list.ListKind;
+        //             gameList.ListKindId = list.ListKindId;
+
+        //             loggedInUser.PersonGameLists.Add(gameList);
+        //             _personRepository.AddOrUpdate(loggedInUser);
+        //         }
+        //         return Ok();
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Debug.WriteLine(e);
+        //         return BadRequest(e.Message);
+        //     }
+        // }
+
+        // [HttpPost]
+        // public async Task<ActionResult<IgdbGame>>  AddGameToDb(Game game)
+        // {
+        //     try
+        //     {
+        //         _gameRepository.AddOrUpdate(game);
+        //         return Ok();
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Debug.WriteLine(e);
+        //         return BadRequest(e);
+        //     }
+        // }
     }
 }
--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/addGame.js
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/addGame.js
@@ -2,52 +2,52 @@ console.log("Hello from addGame.js");
 
 $(document).ready( function () {
     $("table").on("click", "button", function () {
-        let id = $(this).attr("id");
-        let tableDataId = $(`#td${id}`).attr("id");
-        const title = $(`#${tableDataId}`).text();
-    
-        console.log(id);
-        console.log(tableDataId);
-        console.log(title);
-        //
+        const $row = $(this).closest("tr");
+        // * retrieve info from from tag
+        const $name = $row.find("b");
+        
+        console.log(`$row:${$row}`);
+        console.log(`$name:${$name.text()}`);
+
         $.ajax({
             type: "GET",
             url: "/api/Game/getUserLists",
             dataType: "json",
-            success: function (data) {
-                console.log("Getting user lists succeeded");
-                let listNameFormId = $("#listName").attr("id");
-                $("#listName").empty();
-                for (let i = 0; i < data.length; ++i) {
-                    console.log(data[i]);
-                    let selectOption = `<option value="${data[i].listName}">${data[i].listName}</option>`;
-                    $("#listName").append(selectOption);
-                }
-    
-                $("#formSubmit").on("click", function () {
-                    const list = $("#listName").text();
-                    console.log("listName = " + list);
-                    let origin = $(location).attr("origin");
-                    console.log(title);
-                    const dataToSend = { listName: list, gameTitle: title };
-                    console.log(dataToSend);
-                    $.ajax({
-                        type: "POST",
-                        dataType: "json",
-                        url: `${origin}/api/Game/addGame`,
-                        contentType: "application/json; charset=UTF-8",
-                        data: JSON.stringify(dataToSend),
-                        success: afterAddGame,
-                        error: errorAlert
-                    });
-                });
-            },
+            success: getUserListsSuccess,
             error: getUserListsFailure
         });
     
     });
 });
 
+// function (data) {
+//     console.log("Getting user lists succeeded");
+//     let listNameFormId = $("#listName").attr("id");
+//     $("#listName").empty();
+//     for (let i = 0; i < data.length; ++i) {
+//         console.log(data[i]);
+//         let selectOption = `<option value="${data[i].listName}">${data[i].listName}</option>`;
+//         $("#listName").append(selectOption);
+//     }
+
+//     $("#formSubmit").on("click", function () {
+//         const list = $("#listName").text();
+//         console.log("listName = " + list);
+//         let origin = $(location).attr("origin");
+//         console.log(title);
+//         const dataToSend = { listName: list, gameTitle: title };
+//         console.log(dataToSend);
+//         $.ajax({
+//             type: "POST",
+//             dataType: "json",
+//             url: `${origin}/api/Game/addGame`,
+//             contentType: "application/json; charset=UTF-8",
+//             data: JSON.stringify(dataToSend),
+//             success: afterAddGame,
+//             error: errorAlert
+//         });
+//     });
+// }
 function addGame() {
 
 }
@@ -59,7 +59,7 @@ function getUserListsSuccess(data) {
     $("#listName").empty();
     for (let i = 0; i < data.length; ++i) {
         console.log(data[i]);
-        let selectOption = `<option value="${data[i].listName}">${data[i].listName}</option>`;
+        let selectOption = `<option value="${data[i].listKind}">${data[i].listKind}</option>`;
         $("#listName").append(selectOption);
     }
 
