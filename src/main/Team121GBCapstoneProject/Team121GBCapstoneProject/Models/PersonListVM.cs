d455ee4db23905a3216d64bdb3824b645d027305
Nathaniel Kuga <natelkuga@protonmail.com>
2023-03-16 15:11:28 -0700

    Nathaniel: addded the constructor.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Models/PersonListVM.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Models/PersonListVM.cs
@@ -3,22 +3,11 @@ namespace Team121GBCapstoneProject.Models;
 public class PersonListVM
 {
     public string ListKind { get; set; }
+    public List<PersonGame> PersonGames { get; set; }
     public PersonListVM(string listKind, List<PersonGame> games)
     {
         ListKind = listKind;
+        PersonGames = new List<PersonGame>();
+        games.ForEach(g => PersonGames.Add(g));
     }
-
 }
-// var distinctPeople = personLists.Distinct(p => new { p })
-//                                         .ToList();
-    //public List PersonLists { get; set; }
-    // public PersonListVM(List<PersonList> personLists)
-    // {
-    //     PersonLists = personLists.OrderBy(l => l.ListKindId)
-    //                              .GroupBy(l => l.ListKind)
-    //                              .Select(group => group.ToList())
-    //                              .ToList();
-    //     ListKind = personLists.Select(l => l.ListKind)
-    //                           .Distinct()
-    //                           .ToList();
-    // }
