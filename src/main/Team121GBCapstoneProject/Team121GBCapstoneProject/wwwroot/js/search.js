ca9a337482610ac7b00347fe3624281362cd0657
Nathaniel Kuga <natelkuga@protonmail.com>
2023-04-05 19:55:32 -0700

    Nathaniel: updated the view to include description.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/search.js
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/search.js
@@ -22,7 +22,8 @@ function displaySearchResults(query) {
                         var row = `<tr>
                                     <td><img src="${resizedCoverArt}"></td>
                                     <td><b>${game.gameTitle}</b></td>
-                                    <td><a href="${game.gameWebsite}">${game.gameWebsite}"</a></td>
+                                    <td><a class="btn btn-primary" href="${game.gameWebsite}">IGDB Page</a></td>
+                                    <td><p>${game.gameDescription}</p></td>
                                     <td><button id="${i}" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddGame">Add Game</button></td>
                                </tr>`;
                         $("#gameTableBody").append(row);
