1e9b2ffcfdfa659abaaf8eec33956e8922b77c68
CyberGecko64 <Qroan@my.chemeketa.edu>
2023-04-10 17:52:53 -0700

    Quinton: Created new tests for Home page and they run. NOTE: The Application must be running to run BDD Testing. This will also include some of scots works so it may look different from my code.

--- src/main/Team121GBCapstoneProject/Team121GB_BDD_Test/StepDefinitions/HomeStepDefinitions.cs
+++ src/main/Team121GBCapstoneProject/Team121GB_BDD_Test/StepDefinitions/HomeStepDefinitions.cs
@@ -1,3 +1,7 @@
+using AspNetCore;
+using NUnit.Framework;
+using Standups_BDD_Tests.Drivers;
+using Standups_BDD_Tests.PageObjects;
 using System;
 using TechTalk.SpecFlow;
 
@@ -6,6 +10,12 @@ namespace Team121GB_BDD_Test.StepDefinitions
     [Binding]
     public class HomeStepDefinitions
     {
+        private readonly HomePageObject _homePage;
+        public HomeStepDefinitions(BrowserDriver browserDriver) 
+        {
+            _homePage = new HomePageObject(browserDriver.Current);
+        }    
+
         [Given(@"I am a visitor")]
         public void GivenIAmAVisitor()
         {
@@ -13,15 +23,31 @@ namespace Team121GB_BDD_Test.StepDefinitions
         }
 
         [When(@"I am on the ""([^""]*)"" page")]
-        public void WhenIAmOnThePage(string page)
+        public void WhenIAmOnThePage(string pageName)
         {
-            throw new PendingStepException();
+            _homePage.GoTo(pageName);
         }
 
         [Then(@"The page title contains ""([^""]*)""")]
         public void ThenThePageTitleContains(string p0)
         {
-            throw new PendingStepException();
+            _homePage.GetTitle().Should().ContainEquivalentOf(p0, AtLeast.Once());
         }
+
+        [Then(@"The page presents a Create Account button")]
+        public void ThenThePagePresentsACreateAccountButton()
+        {
+            _homePage.RegisterButton.Should().NotBeNull();
+            _homePage.RegisterButton.Displayed.Should().BeTrue();
+        }
+
+        [Then(@"The page presents a Login button")]
+        public void ThenThePagePresentsALoginButton()
+        {
+            _homePage.RegisterButton.Should().NotBeNull();
+            _homePage.RegisterButton.Displayed.Should().BeTrue();
+        }
+
+
     }
 }
