fa07c6e5b7c6aae032d30b9aeb24db7c49a9c535
Nathaniel Kuga <natelkuga@protonmail.com>
2023-04-22 11:03:34 -0700

    Nathaniel: setting up BDD Tests.

---
+++ src/main/Team121GBCapstoneProject/Team121GB_BDD_Test/Features/GP-85-SetDalleImageToProfilePicture.feature
@@ -0,0 +1,45 @@
+﻿Feature: GP-85-SetDalleImageToProfilePicture
+
+**As a user, I would like the ability to set my AI generated image as my profile picture in the app so that  I don't have to manually upload my image.**
+
+This user story is about stream lining the experience users have when they user our apps Dalle image generator service. The version this feature improves on
+makes a user have to download the image and then manualy upload the image. This user story combines those steps into a single button click.
+
+
+@notLoggedIn
+Scenario: A visitor cannot access the Dalle Image generation page
+	Given I am not logged in
+	And I am on the home page
+	And I attempt to access the image generator page,
+	Then I should be ask to login before accessing the page.
+
+@LoggedIn
+Scenario: A logged in user can access the Dalle Image generation page and enter a prompt
+	Given I am a logged in user
+	And I am on the home page
+	And I navigate to the image generator page
+	Then I should see a counter telling me how many image credits I have
+	And I input a prompt
+	And click the Generate Image Button my credits will decrease by 1
+
+@LoggedIn
+Scenario: A logged in user can access the Dalle Image generation page but if they have no credits they cannot generate an image
+	Given I a logged in user
+	And I am on the image generator page
+	And I have no image credits left
+	And I try and generate an image
+	Then I will be told that I can't generate any more images
+
+@LoggedIn
+Scenario: A logged in user can access the Dalle Image generation page and set their profile image to the Dalle generated image
+	Given I am logged in user on the image generator page
+	And I've generated an image
+	When I click the button for setting it as my profile picture
+	Then my profile picture will be updated to display the new profile image
+
+@LoggedIn
+Scenario: A logged in user can access the Dalle Image generation page but there was an error and they are notified
+	Given I am a logged in user on the image generator page
+	And I've enterred a prompt
+	But the image generation failed
+	Then I should be notified that something went wrong.
---
+++ src/main/Team121GBCapstoneProject/Team121GB_BDD_Test/StepDefinitions/GP_85_SetDalleImageToProfilePictureStepDefinitions.cs
@@ -0,0 +1,141 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Team121GB_BDD_Test.StepDefinitions
+{
+    [Binding]
+    public class GP_85_SetDalleImageToProfilePictureStepDefinitions
+    {
+        [Given(@"I am not logged in")]
+        public void GivenIAmNotLoggedIn()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I attempt to access the image generator page,")]
+        public void GivenIAttemptToAccessTheImageGeneratorPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should be ask to login before accessing the page\.")]
+        public void ThenIShouldBeAskToLoginBeforeAccessingThePage_()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am a logged in user")]
+        public void GivenIAmALoggedInUser()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I navigate to the image generator page")]
+        public void GivenINavigateToTheImageGeneratorPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should see a counter telling me how many image credits I have")]
+        public void ThenIShouldSeeACounterTellingMeHowManyImageCreditsIHave()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I input a prompt")]
+        public void ThenIInputAPrompt()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"click the Generate Image Button my credits will decrease by (.*)")]
+        public void ThenClickTheGenerateImageButtonMyCreditsWillDecreaseBy(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I a logged in user")]
+        public void GivenIALoggedInUser()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am on the image generator page")]
+        public void GivenIAmOnTheImageGeneratorPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I have no image credits left")]
+        public void GivenIHaveNoImageCreditsLeft()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I try and generate an image")]
+        public void GivenITryAndGenerateAnImage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I will be told that I can't generate any more images")]
+        public void ThenIWillBeToldThatICantGenerateAnyMoreImages()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am logged in user on the image generator page")]
+        public void GivenIAmLoggedInUserOnTheImageGeneratorPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I've generated an image")]
+        public void GivenIveGeneratedAnImage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click the button for setting it as my profile picture")]
+        public void WhenIClickTheButtonForSettingItAsMyProfilePicture()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"my profile picture will be updated to display the new profile image")]
+        public void ThenMyProfilePictureWillBeUpdatedToDisplayTheNewProfileImage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am a logged in user on the image generator page")]
+        public void GivenIAmALoggedInUserOnTheImageGeneratorPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I've enterred a prompt")]
+        public void GivenIveEnterredAPrompt()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the image generation failed")]
+        public void GivenTheImageGenerationFailed()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should be notified that something went wrong\.")]
+        public void ThenIShouldBeNotifiedThatSomethingWentWrong_()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
