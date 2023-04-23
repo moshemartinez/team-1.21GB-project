6dc03e0823d9b710c1ef795d94824eb38f88d5a0
Nathaniel Kuga <natelkuga@protonmail.com>
2023-04-22 19:37:57 -0700

    Nathaniel: working on seeding users.

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/ProjectDataBase/SeedData.cs
@@ -0,0 +1,27 @@
+﻿namespace Team121GBCapstoneProject.ProjectDataBase;
+
+/// <summary>
+/// Helper class to hold information we need for users in our project databases.
+/// </summary>
+public class UserInfoData
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool EmailConfirmed { get; set; } = true;
+    public int DalleCreditsCount { get; set; } = 5;
+}
+public class SeedData
+{
+    /// <summary>
+    /// Data to be used to see the FujiUsers and ASPNetUsers Tables
+    /// </summary>
+    public static readonly UserInfoData[] UserSeedData = new UserInfoData[]
+    {
+            new UserInfoData { UserName = "TaliaK", Email = "knott@example.com", FirstName = "Talia", LastName = "Knott" },
+            new UserInfoData { UserName = "ZaydenC", Email = "clark@example.com", FirstName = "Zayden", LastName = "Clark", DalleCreditsCount = 0},
+            new UserInfoData { UserName = "DavilaH", Email = "hareem@example.com", FirstName = "Hareem", LastName = "Davila" },
+            new UserInfoData { UserName = "KrzysztofP", Email = "krzysztof@example.com", FirstName = "Krzysztof", LastName = "Ponce" }
+    };
+}
---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Utilities/SeedUsers.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Team121GBCapstoneProject.ProjectDataBase;
+using Team121GBCapstoneProject.Models;
+using Team121GBCapstoneProject.Areas.Identity.Data;
+
+
+namespace Team121GBCapstoneProject.Utilities;
+public class SeedUsers
+{
+    /// <summary>
+    /// Initialize seed data for users.  Creates users for login using Identity and also application users.  One password
+    /// is used for all accounts.
+    /// </summary>
+    /// <param name="serviceProvider">The fully configured service provider for this app that can provide a UserManager and the applications dbcontext</param>
+    /// <param name="seedData">Array of seed data holding all the attributes needed to create the user objects</param>
+    /// <param name="testUserPw">Password for all seed accounts</param>
+    /// <returns></returns>
+    public static async Task Initialize(IServiceProvider serviceProvider, UserInfoData[] seedData, string testUserPw)
+    {
+        try
+        {
+            // Get our application db context
+            //   For later reference -- this uses the "Service Locator anti-pattern", not usually a good pattern
+            //   but unavoidable here
+            using (var context = new GPDbContext(serviceProvider.GetRequiredService<DbContextOptions<GPDbContext>>()))
+            {
+                // Get the Identity user manager
+                var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                foreach (var u in seedData)
+                {
+                    // Ensure this user exists or is newly created (Email is used for username since that is the default in Register and Login -- change those and then use username here if you want it different than email
+                    var identityID = await EnsureUser(userManager, testUserPw, u.Email, u.Email, u.EmailConfirmed);
+                    // Create a new FujiUser if this one doesn't already exist
+                    ApplicationUser applicationUser = new ApplicationUser { FirstName = u.FirstName, LastName = u.LastName };
+                    if (!context.People.Any(x => x.AuthorizationId == identityID))
+                    {
+                        // Doesn't already exist, so add a new user
+                        context.Add(applicationUser);
+                        await context.SaveChangesAsync();
+
+                        Person person = await context.People.FirstOrDefaultAsync(p => p.AuthorizationId == identityID);
+                        person.DallECredits = u.DalleCreditsCount;
+                    }
+                }
+            }
+        }
+        catch (InvalidOperationException ex)
+        {
+            // Thrown if there is no service of the type requested from the service provider
+            // Catch it (and don't throw the exception below) if you don't want it to fail (5xx status code)
+            throw new Exception("Failed to initialize user seed data, service provider did not have the correct service");
+        }
+    }
+    /// <summary>
+    /// Helper method to ensure that the Identity user exists or has been newly created.  Modified from
+    /// <a href="https://docs.microsoft.com/en-us/aspnet/core/security/authorization/secure-data?view=aspnetcore-5.0#create-the-test-accounts-and-update-the-contacts">create the test accounts and update the contacts (in Contoso University example)</a>
+    /// </summary>
+    /// <param name="userManager"></param>
+    /// <param name="password"></param>
+    /// <param name="username"></param>
+    /// <param name="email"></param>
+    /// <param name="emailConfirmed"></param>
+    /// <returns>The Identity ID of the user</returns>
+    private static async Task<string> EnsureUser(UserManager<IdentityUser> userManager, string password, string username, string email, bool emailConfirmed)
+    {
+        var user = await userManager.FindByNameAsync(username);
+        if (user == null)
+        {
+            user = new IdentityUser
+            {
+                UserName = username,
+                Email = email,
+                EmailConfirmed = emailConfirmed
+            };
+            await userManager.CreateAsync(user, password);
+        }
+
+        if (user == null)
+        {
+            throw new Exception("The password is probably not strong enough!");
+        }
+
+        return user.Id;
+    }
+}
