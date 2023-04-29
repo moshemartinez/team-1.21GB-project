9970f5d85b365a3aea3355c672a2ef13f161af49
Nathaniel Kuga <natelkuga@protonmail.com>
2023-04-28 20:17:01 -0700

    Added a controller for the ChatGptService class

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Controllers/ChatGptController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels.ResponseModels;
+using Team121GBCapstoneProject.Areas.Identity.Data;
+using Team121GBCapstoneProject.DAL.Abstract;
+using Team121GBCapstoneProject.Models;
+using Team121GBCapstoneProject.Services.Abstract;
+
+namespace Team121GBCapstoneProject.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatGptController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IChatGptService _chatGptService;
+
+    public ChatGptController(UserManager<ApplicationUser> userManager,
+                                         IChatGptService chatGptService)
+    {
+        _userManager = userManager;
+        _chatGptService = chatGptService;
+    }
+
+    [HttpGet("GetChatResponse")]
+    public async Task<ActionResult<string>> GetChatResponse(string prompt)
+    {
+        try
+        {
+            string response = "";
+            if (String.IsNullOrEmpty(prompt) is false)
+            {
+                response = await _chatGptService.GetChatResponse(prompt);
+            }
+            return Ok(response);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+}
