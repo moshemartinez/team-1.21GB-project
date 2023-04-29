736d1d241c6b8412eabcaee428c6c2e9d667f5d0
Nathaniel Kuga <natelkuga@protonmail.com>
2023-04-29 09:53:51 -0700

    Adding Chat bot to the view

---
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/chatGpt.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function () {
+    $('#send').on('click', function () {
+        // let prompt = $('#prompt').val();
+        // sendMessage(prompt);
+        sendMessage($('#prompt').val());
+    });
+    // $('#prompt').on('keydown', function(event) {
+    //     if (event.keyCode === 13) { // 13 is the code for the Enter key
+    //         event.preventDefault(); // prevent the default behavior of the enter key
+    //         // $('#send').click(); // trigger the click event on the #send button
+    //         sendMessage($('#prompt').val());
+    //     }
+    // });
+});
+
+function sendMessage(prompt) {
+    $.ajax({
+        url: '/api/ChatGpt/GetChatResponse',
+        method: 'GET',
+        data: {
+            prompt: prompt
+        },
+        success: sendMessageSuccess,
+        error: (error) => {
+            console.log(error);
+        }
+    });
+}
+
+function sendMessageSuccess(data) {
+    console.log(data);
+    document.getElementById("responses").innerHTML += "<p>" + data + "</p>";
+}
