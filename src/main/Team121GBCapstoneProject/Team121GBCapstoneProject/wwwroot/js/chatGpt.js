360eee9f23c727be293c5d8a508dbc8f05f65fab
Nathaniel Kuga <natelkuga@protonmail.com>
2023-04-29 18:24:06 -0700

    Got my working working as expected.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/chatGpt.js
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/wwwroot/js/chatGpt.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             return;
         }
         $('#prompt').val('');
-        // addUserQueryToDOM($prompt);
+        addUserQueryToDOM($prompt);
         sendMessage($prompt);
     });
     $("#prompt").keypress(function(event) {
@@ -22,95 +22,48 @@ $(document).ready(function () {
                 return;
             }
             $('#prompt').val('');
-            // addUserQueryToDOM($prompt);
+            addUserQueryToDOM($prompt);
             sendMessage($prompt);
         }
       });
 });
 
-function sendMessage(prompt) {
-    $.ajax({
+//q: will this make the javascript wait till the ajax call returns?
+//a: no, it will not
+//q: why not?
+//a: because the ajax call is async
+//q: how can I make it wait for the ajax call to finsh?
+//a: use async: false in the ajax call
+
+async function sendMessage(prompt) {
+    await $.ajax({
         url: '/api/ChatGpt/GetChatResponse',
         method: 'GET',
         data: {
             prompt: prompt
         },
         success: sendMessageSuccess,
-        // /error: console.log("erro//)
+        error: sendMessageError
     });
 }
-         
-function sendMessageSuccess(data) {
+
+async function sendMessageSuccess(data) {
     $('#loadingChatGif').attr('hidden', 'true');
     console.log(data);
     let $responses = $('#responses');
-    let msg = $('<div>').html(data)
-                        .addClass('card text-white bg-primary');
+    let $row = $('<div></div>').addClass('card text-white bg-primary');
+    await $responses.append($row);
     typeLetter(data);
-    $responses.append(msg);
 }
-// function sendMessageSuccess(data) {
-//     $('#loadingChatGif').attr('hidden', 'true'); 
-//     console.log(data);
-//     let $responses = $('#responses');
-//     $responses.append('<div class="row"></div>');
-//     let msg = $('<div>').html(data)
-//                         .addClass('card text-white bg-primary')
-//                         .css('max-width', '30rem')
-//                         .css('min-width', '15rem')
-//                         .css('justify-content', 'center')
-//                         .css('align-items', 'center');
-//     $('#responses').children()
-//                    .last()
-//                    .append(msg);
-//     typeLetter(data);
-//     // $('#responses').append('<br/>');
-// }
-// function sendMessageSuccess(data) {
-//     document.querySelector('#loadingChatGif').setAttribute('hidden', 'true');
-//     console.log(data);
-//     let responses = document.querySelector('#responses');
-//     let newRow = document.createElement('div');
-//     newRow.classList.add('row');
-//     responses.appendChild(newRow);
-//     let newMsg = document.createElement('div');
-//     newMsg.innerHTML = data;
-//     newMsg.classList.add('card', 'text-white', 'bg-primary');
-//     newMsg.style.maxWidth = '30rem';
-//     newMsg.style.minWidth = '15rem';
-//     newMsg.style.justifyContent = 'center';
-//     newMsg.style.alignItems = 'center';
-//     // let lastChild = responses.lastElementChild;
-//     newRow.appendChild(newMsg);
-//     typeLetter(data);
-// }
 
-// function sendMessageSuccess(data) {
-//     $('#loadingChatGif').attr('hidden', 'true');
-//     console.log(data);
-//     let $responses = $('#responses');
-//     $responses.append(`<div class="row">
-//     <div class="col-sm-6">
-//       <div class="card">
-//         <div class="card-body">
-//           <h5 class="card-title">Special title treatment</h5>
-//           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-//           <a href="#" class="btn btn-primary">Go somewhere</a>
-//         </div>
-//       </div>
-//     </div>`);
-// }
-
-// function sendMessageError(data) {
-//     $('#loadingChatGif').attr("hidden", "true"); 
-//     console.log(data);
-//     let $responses = $('#responses');
-//     $responses.append('<div class="row"></div>');
-    
-//     $('#responses').append('<div class="card text-white bg-danger" style="max-width: 15rem;  justify-content: center; align-items: center;"></div>');
-//     typeLetter(data);
-//     $('#responses').append('<br/>');
-// }
+async function sendMessageError(data) {
+    $('#loadingChatGif').attr('hidden', 'true');
+    console.log(data);
+    let $responses = $('#responses');
+    let $row = $('<div></div>').addClass('card text-white bg-danger');
+    await $responses.append($row);
+    typeLetter(data);
+}
 
 function typeLetter(data) {
     const text = data.split(' ');
@@ -124,9 +77,9 @@ function typeLetter(data) {
     });
 }
 
-// function addUserQueryToDOM(query) {
-//                                         $('#responses').append('<div class="card text-white bg-primary" style="max-width: 30rem; min-width: 15rem; justify-content: center;"></div>');
-
-//     $('#responses').append(`<div class="card" style="background-image: linear-gradient(white, white); max-width: 30rem; min-width: 15rem;  justify-content: center; align-items: center;">${query}</div>`);
-//     $('#responses').append('<br/>');
-// }
+function addUserQueryToDOM(prompt) {
+    let $responses = $('#responses');
+    let msg = $('<div>').html(prompt)
+                        .addClass('card text-white bg-secondary');
+    $responses.append(msg);
+}
