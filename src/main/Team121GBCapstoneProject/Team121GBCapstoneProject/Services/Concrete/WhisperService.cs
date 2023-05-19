56e4bb937310ffd7ea113fb8553420addf322421
Nathaniel Kuga <natelkuga@protonmail.com>
2023-05-19 15:04:10 -0700

    Cleaned up the code in the Whisper service class, controller and got rid of outdated tests.

--- src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Services/Concrete/WhisperService.cs
+++ src/main/Team121GBCapstoneProject/Team121GBCapstoneProject/Services/Concrete/WhisperService.cs
@@ -14,37 +14,16 @@ public class WhisperService : IWhisperService
     private readonly IOpenAIService _openAIService;
     public WhisperService(IOpenAIService openAIService) => _openAIService = openAIService;
 
-
-    public void SaveByteArrayAsMp3(byte[] byteArray, string filePath)
-    {
-        try
-        {
-            using (FileStream fs = new FileStream(filePath, FileMode.Create))
-            {
-                fs.Write(byteArray, 0, byteArray.Length);
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-            throw new Exception(ex.Message);
-        }
-
-    }
     public async Task<string> GetTextFromSpeech(IFormFile file)
     {
-        //?look into windows temp files in c#
-        //if (audioMp3 is null or { Length: 0 }) throw new ArgumentNullException(nameof(audioMp3), "The audioMp3 byte array is null or empty.");
+        if (file is null or { Length: 0 }) throw new ArgumentNullException(nameof(file), "The file is null or empty.");
 
-        string fileName = Path.GetTempFileName();
-        // string fileName = "audio.mp3";
-        //SaveByteArrayAsMp3(audioMp3, fileName);
-        //byte[] file = await File.ReadAllBytesAsync(fileName);
-        //fileName = await File.ReadAllBytesAsync(audioMp3);
-        //await File.ReadAllBytesAsync(fileName, audioMp3);
+        // * read out the file into a byte array
         MemoryStream ms = new MemoryStream();
         await file.CopyToAsync(ms);
         byte[] fileBytes = ms.ToArray();         
+        
+        // * send the byte array to the OpenAI API
         var audioResult = await _openAIService.Audio.CreateTranscription(new AudioCreateTranscriptionRequest()
         {
             FileName = file.FileName,
@@ -67,41 +46,4 @@ public class WhisperService : IWhisperService
             throw new Exception(audioResult.Error.Message);
         }
     }
-    // look up how to create a file in c# 
-    // check that the incoming file is valid
-
-    //public async Task<string> GetTextFromSpeech(byte[] audioMp3)
-    //{
-    //    //?look into windows temp files in c#
-    //    if (audioMp3 is null or { Length: 0 }) throw new ArgumentNullException(nameof(audioMp3), "The audioMp3 byte array is null or empty.");
-
-    //    string fileName = Path.GetTempFileName();
-    //    // string fileName = "audio.mp3";
-    //    //SaveByteArrayAsMp3(audioMp3, fileName);
-    //    byte[] file = await File.ReadAllBytesAsync(fileName);
-    //    //fileName = await File.ReadAllBytesAsync(audioMp3);
-    //    //await File.ReadAllBytesAsync(fileName, audioMp3);
-    //    //MemoryStream ms = new MemoryStream(file);
-    //    var audioResult = await _openAIService.Audio.CreateTranscription(new AudioCreateTranscriptionRequest()
-    //    {
-    //        FileName = "recording.mp3",
-    //        File = audioMp3,
-    //        Model = WhisperV1,
-    //        ResponseFormat = StaticValues.AudioStatics.ResponseFormat.VerboseJson
-    //    });
-    //    if (audioResult.Successful)
-    //    {
-    //        Debug.WriteLine(string.Join("\n", audioResult.Text));
-    //        return audioResult.Text;
-    //    }
-    //    else
-    //    {
-    //        if (audioResult.Error == null)
-    //        {
-    //            throw new Exception("Unknown Error returned from OpenAI API.");
-    //        }
-    //        Debug.WriteLine($"{audioResult.Error.Code}: {audioResult.Error.Message}");
-    //        throw new Exception(audioResult.Error.Message);
-    //    }
-    //}
 }
